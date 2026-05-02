eb1719effef9a28e3dea3c5a14f132ed57de65f9
Javier Vallejos <51408883+jnvallejos@users.noreply.github.com>
2026-05-02 12:31:52 -0300

    chore(solution): add Android Gradle project skeleton

---
+++ android/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.25" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.7" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
+}
